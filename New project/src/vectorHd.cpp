206a2dc832c5b62863aecc2864767e23270d7b6a
cern <70802909+cern143@users.noreply.github.com>
2023-02-12 11:27:57 -0500

    Add files via upload

--- New project/src/vectorHd.cpp
+++ New project/src/vectorHd.cpp
@@ -31,47 +31,46 @@ void readHd(vector<Hopdong> &v, int mode) {
     }
 }
 
-void searchHd(vector<Hopdong> &v, string s, vector<int> &rank, int mode) {
+void searchHd(vector<Hopdong*> v, string s, vector<int> &rank, int mode) {
     //cout << "Ket qua tim kiem: \n";
     switch (mode) {
     case 1:
         for (int i = 0; i < v.size(); i++)
         {
-            if ((v.at(i).hoten.find(s)) != string::npos)
+            if ((v.at(i)->hoten.find(s)) != string::npos)
             {
                 rank.push_back(i);
                 //cout << rank.size() + 1 << "\n";
-                v.at(i).show();
+                v.at(i)->show();
             }
         }
         break;
     case 2:
         for (int i = 0; i < v.size(); i++)
         {
-            if ((v.at(i).bienso.find(s)) != string::npos)
+            if ((v.at(i)->bienso.find(s)) != string::npos)
             {
                 rank.push_back(i);
                 //cout << rank.size() + 1 << "\n";
-                v.at(i).show();
+                v.at(i)->show();
             }
         }
         break;
     case 3:
         for (int i = 0; i < v.size(); i++)
         {
-            if ((v.at(i).loaixe.find(s)) != string::npos)
+            if ((v.at(i)->loaixe.find(s)) != string::npos)
             {
                 rank.push_back(i);
                 //cout << rank.size() + 1 << "\n";
-                v.at(i).show();
+                v.at(i)->show();
             }
         }
         break;
     }
 }
 
-void changeHd(vector<Hopdong> &v, vector<int> &rank)
-{
+void changeHd(vector<Hopdong> &v, vector<int> &rank) {
     int stt;
     //cout << "Chon hop dong ban muon sua (Nhap so thu tu hien thi cua hop dong):\n";
     //cout << "Lua chon cua ban: ";
@@ -90,13 +89,23 @@ void deleteHd(vector<Hopdong> &v, vector<int> &rank)
     //cout << "Xoa hop dong thanh cong!";
 }
 
-unsigned int sum(vector<Hopdong*> v, struct tm start_time, struct tm end_time) {
+unsigned int cost_sum(vector<Hopdong*> v, struct tm start_time, struct tm end_time) {
     unsigned int sum;
     time_t x = mktime(&start_time);
     time_t y = mktime(&end_time);
     for ( int i = 0; i < v.size(); i++ ) {
         if ( x <=  mktime(&(v.at(i)->start_time)) && mktime(&(v.at(i)->end_time)) <= y ) {
             sum = sum + v.at(i)->cost();
+        }
+    }
+    return sum;
+}
+
+void cost_search(vector<Hopdong*> v, vector<int> &rank, int cost) {
+    for ( int i = 0; i < v.size(); i++ ) {
+        if ( v.at(i)->cost() > cost ) {
+            rank.push_back(i);
+            v.at(i)->show();
         }
     }
 }
