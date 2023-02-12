17fe09037adb8e1807e168977875552677257b6a
cern <70802909+cern143@users.noreply.github.com>
2023-02-12 11:28:58 -0500

    Add files via upload

--- New project/include/vectorHd.h
+++ New project/include/vectorHd.h
@@ -6,4 +6,5 @@ void readHd(vector<Hopdong> &v, int mode);
 void searchHd(vector<Hopdong> &v, string s, vector<int> &rank, int mode);
 void changeHd(vector<Hopdong> &v, vector<int> &rank);
 void deleteHd(vector<Hopdong> &v, vector<int> &rank);
-unsigned int sum(vector<Hopdong> &v, struct tm start_time, struct tm end_time);
+unsigned int cost_sum(vector<Hopdong*> v, struct tm start_time, struct tm end_time);
+void cost_search(vector<Hopdong*> v, vector<int> &rank, int cost);
