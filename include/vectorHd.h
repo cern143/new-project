65a2a268ced32bb2ee457bc4322d5651de79a3f5
cern <vkingpro35@gmai.com>
2023-02-13 02:57:52 -0500

    commit

--- include/vectorHd.h
+++ include/vectorHd.h
@@ -6,7 +6,7 @@ void readHd(vector<Hd_ngay> &v);
 void readHd(vector<Hd_thang> &v);
 void searchHd(vector<Hopdong*> v, string s, vector<int> &rank, int mode);
 void changeHd(vector<Hopdong> &v, vector<int> &rank);
-void deleteHd(vector<Hopdong> &v, vector<int> &rank);
+void deleteHd(vector<Hopdong*> v, vector<int> &rank);
 unsigned int cost_sum(vector<Hopdong*> v, struct tm start_time, struct tm end_time);
 void cost_search(vector<Hopdong*> v, vector<int> &rank, int cost);
 int stt(vector<Hopdong*> v, string hoten);
--- src/date.cpp
+++ src/date.cpp
@@ -8,6 +8,13 @@ unsigned int diffdays(struct tm start, struct tm end)
     time_t x, y;
     double difference;
     start.tm_hour = 0;
+    start.tm_min = 0;
+    start.tm_sec = 0;
+    start.tm_year = start.tm_year - 1900;
+    end.tm_hour = 0;
+    end.tm_min = 0;
+    end.tm_sec = 0;
+    end.tm_year = end.tm_year - 1900;
     x = mktime(&start);
     y = mktime(&end);
     if (x != (time_t)(-1) && y != (time_t)(-1))
